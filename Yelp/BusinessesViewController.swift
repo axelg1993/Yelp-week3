9288b4f3ac96bc1c02d717726b3abea0ced47fae
axelg1993 <axelg1993@gmail.com>
2016-02-27 22:46:32 -0500

    code is set

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -2,27 +2,49 @@
 //  BusinessesViewController.swift
 //  Yelp
 //
-//  Created by Timothy Lee on 4/23/15.
+//  Created by Timothy Lee and Axel Guzman on 4/23/15 and 2/26/16, respectively.
 //  Copyright (c) 2015 Timothy Lee. All rights reserved.
 //
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
     var businesses: [Business]!
+    var searchBar: UISearchBar!
+    var filteredData: [Business]!
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
-            self.businesses = businesses
+         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
         
+        
+        searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+
+        filteredData = businesses
+        
+        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+            
+            self.filteredData = businesses
+            self.businesses = businesses
+            self.tableView.reloadData()
+            
             for business in businesses {
                 print(business.name!)
                 print(business.address!)
             }
         })
+        
+        
 
 /* Example of Yelp search with more search options specified
         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
@@ -33,13 +55,71 @@ class BusinessesViewController: UIViewController {
                 print(business.address!)
             }
         }
-*/
+       */
+
+        
+      }
+ 
+ 
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
     }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+  
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if filteredData != nil {
+            return filteredData!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
+        cell.business = filteredData[indexPath.row]
+        return cell
+    }
+
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        if searchText.isEmpty {
+            filteredData = businesses
+        } else {
+            // The user has entered text into the search box
+            // Use the filter method to iterate over all items in the data array
+            // For each item, return true if the item should be included and false if the
+            // item should NOT be included
+            filteredData = businesses.filter({(dataItem: Business) -> Bool in
+                // If dataItem matches the searchText, return true to include it
+                if dataItem.name!.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil {
+                    return true
+                } else {
+                    return false
+                }
+            })
+        }
+        tableView.reloadData()
+    }
+    
+    
+
+    
+    
+    
+        
+        // Dispose of any resources that can be recreated.
+
 
     /*
     // MARK: - Navigation
@@ -50,5 +130,6 @@ class BusinessesViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
 
 }
--- Yelp/YelpClient.swift
+++ Yelp/YelpClient.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-
 import AFNetworking
 import BDBOAuth1Manager
 
